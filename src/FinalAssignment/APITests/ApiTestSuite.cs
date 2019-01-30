b46b6d7fc40fb43849665d4d0ec096b82dc9725b
Andrii <stellaralnilam@gmail.com>
2019-01-30 23:49:00 +0000

    Added RestSharp Controllers and tests.

---
+++ src/FinalAssignment/APITests/ApiTestSuite.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using FinalAssignment.Utils;
+using NUnit.Framework;
+
+namespace FinalAssignment.APITests
+{
+    [TestFixture]
+    class ApiTestSuite : ApiController
+    {
+
+        [SetUp]
+        public void SetUp()
+        {
+            Initialize("https://jsonplaceholder.typicode.com/");
+        }
+
+        [Test]
+        public void ApiGetFirstPostTitle()
+        {
+            var response = GetRequest("posts/1");
+            ValidateAPIResponse(response, "title", "sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
+        }
+
+        [Test]
+        public void ApiCreateNewPhoto()
+        {
+            var response = PostRequest("photos",
+                new
+                {
+                    albumId = 1,
+                    title = "MyPhotoTest",
+                    url = "https://via.placeholder.com/600/92c952",
+                    thumbnailUrl = "https://via.placeholder.com/150/92c952"
+                });
+
+            ValidateAPIResponse(response, "id", "5001");
+        }
+
+        [Test]
+        public void ApiPatchCreatedPhoto()
+        {
+            var response = PatchRequest("photos/5000",
+                new
+                {
+                    title = "MyPhotoTestUpdated"
+                });
+
+            ValidateAPIResponse(response, "title", "MyPhotoTestUpdated");
+        }        
+    }
+}
---
+++ src/FinalAssignment/Utils/ApiController.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Net;
+using System.Linq;
+using FluentAssertions;
+
+namespace FinalAssignment.Utils
+{
+    class ApiController
+    {
+        private string BaseUrl;
+        private RestClient Client;
+        private RestRequest Request;
+        private IRestResponse Response;       
+        
+        public void Initialize(string baseUrl)
+        {
+            BaseUrl = baseUrl;
+            Client = new RestClient
+            {
+                BaseUrl = new System.Uri(BaseUrl),
+                Timeout = System.Convert.ToInt32(System.TimeSpan.FromMinutes(3).TotalMilliseconds)
+            };
+        }
+
+        public JObject GetRequest(string parameter)
+        {
+            Request = new RestRequest(parameter, Method.GET);
+            Response = Client.Get(Request);
+            Response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            return JObject.Parse(Response.Content);
+        }
+
+        public JObject PostRequest(string parameter, object body)
+        {
+            Request = new RestRequest(parameter, Method.POST);
+            Request.AddJsonBody(body);
+            Response = Client.Post(Request);
+            Response.StatusCode.Should().Be(HttpStatusCode.Created);
+
+            return JObject.Parse(Response.Content);
+        }
+
+        public JObject PatchRequest(string parameter, object body)
+        {
+            Request = new RestRequest(parameter, Method.PATCH);
+            Request.AddJsonBody(body);
+            Response = Client.Patch(Request);
+            Response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            return JObject.Parse(Response.Content);
+        }
+
+        public void ValidateAPIResponse(JObject response, string token, string expectedValue)
+        {
+            string value = (string)response.SelectTokens(token).FirstOrDefault();
+            value.Should().BeEquivalentTo(expectedValue);
+        }
+    }
+}
